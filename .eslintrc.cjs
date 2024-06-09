af4bdc4a46160a3f98ffe90b4f0f0fd98b73ae12
Lyndon Baylin Jr. <lyndon@syntacticsinc.com>
2024-06-09 19:30:13 +0800

    chore: initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react-hooks/exhaustive-deps': 0,
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+};
