6589a79e78468a6c80690e1a6bde13b02bd5002f
Lyndon Baylin Jr. <lyndon@syntacticsinc.com>
2024-06-16 03:45:30 +0800

    ci: font configuration support for inter variable font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'selector',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: [
+        ['InterVariable', ...defaultTheme.fontFamily.sans],
+        { fontFeatureSettings: '"cv11", "ss01"' },
+      ],
+      mono: [[...defaultTheme.fontFamily.mono]],
+    },
     extend: {},
   },
   plugins: [],
