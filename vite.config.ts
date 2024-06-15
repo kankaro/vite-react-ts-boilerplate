ddf2c9dab24da696a9d0f5fe7177c160955d23c6
Lyndon Baylin Jr. <lyndon@syntacticsinc.com>
2024-06-16 03:42:44 +0800

    ci: added import path configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
+
+import aliasImportChecker from 'vite-plugin-alias-import-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), aliasImportChecker({ level: 'error' })],
+  // @source https://vitejs.dev/guide/build.html#library-mode
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+      '@components': fileURLToPath(
+        new URL('./src/components', import.meta.url),
+      ),
+      '@hooks': fileURLToPath(new URL('./src/hooks', import.meta.url)),
+      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
+      '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
+      '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
+    },
+  },
 });
