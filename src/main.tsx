86666be097f8768931a0acb6c1e7f88ce433d966
Lyndon Baylin Jr. <lyndon@syntacticsinc.com>
2024-06-16 03:48:40 +0800

    perf: file import enhancement

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+
+import '@styles/font.css';
+import '@styles/main.css';
+
+import { App } from './pages/App/Loadable';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
