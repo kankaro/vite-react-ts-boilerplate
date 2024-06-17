6d03908600aa251fd505e7906f8f20d0e066d163
Lyndon Baylin Jr. <lyndon@syntacticsinc.com>
2024-06-17 21:26:15 +0800

    test(config): setup test coverage

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { coverageConfigDefaults } from 'vitest/config';
 
 import aliasImportChecker from 'vite-plugin-alias-import-checker';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -13,5 +14,34 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: ['src/__tests__/setup.ts'],
+    reporters: ['default', 'html'],
+    coverage: {
+      enabled: true,
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      ignoreEmptyLines: true,
+      thresholds: {
+        lines: 75,
+        functions: 75,
+        branches: 75,
+        statements: 75,
+      },
+      // Clean coverage results before running tests
+      clean: true,
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      // List of files excluded from coverage as glob patterns.
+      exclude: [
+        'html/**',
+        '.husky/**',
+        '.vscode/**',
+        'node_modules/**',
+        '**/{tailwind,postcss,commitlint}.config.*',
+        '**/.{idea,git,cache,output,temp,editorconfig,gitattributes,gitignore,lintstagedrc,npmrc,yarnrc,nvmrc,prettierignore,}/**',
+        'src/__tests__/setup.ts',
+        'src/assets/**',
+        'src/styles/**',
+        ...coverageConfigDefaults.exclude,
+      ],
+    },
   },
 });
