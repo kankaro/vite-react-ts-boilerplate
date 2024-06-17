3120fe480fee21dac476f4180c8587b38ece87dd
Lyndon Baylin Jr. <lyndon@syntacticsinc.com>
2024-06-17 09:41:01 +0800

    refactor: added a store provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelmetProvider } from 'react-helmet-async';
+import { Provider } from 'react-redux';
 
 import '@styles/font.css';
 import '@styles/main.css';
 
-import { App } from './pages/App/Loadable';
+import { store } from './store';
+
+import { App } from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
-  <HelmetProvider>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </HelmetProvider>,
+  <Provider store={store}>
+    <HelmetProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </HelmetProvider>
+  </Provider>,
 );
