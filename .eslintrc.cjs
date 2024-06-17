7bdc3a21969f271b7ce324cb5b44c8407e999043
Lyndon Baylin Jr. <lyndon@syntacticsinc.com>
2024-06-17 23:26:42 +0800

    refactor(linter): removed `no-restricted-imports linting rule in favor for custom hooks

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -30,15 +30,5 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
-    'no-restricted-imports': 'off',
-    '@typescript-eslint/no-restricted-imports': [
-      'warn',
-      {
-        name: 'react-redux',
-        importNames: ['useSelector', 'useDispatch'],
-        message:
-          'Use typed hooks `useAppDispatch` and `useAppSelector` instead.',
-      },
-    ],
   },
 };
