77827babdd5bd34e606707f6689a13a68151d515
Lyndon Baylin Jr. <lyndon@syntacticsinc.com>
2024-06-17 09:31:58 +0800

    refactor(config): `resolve` settings removed in favor for `vite-tsconfig-paths`

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,17 @@
 /// <reference types="vitest" />
 
 import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 
 import aliasImportChecker from 'vite-plugin-alias-import-checker';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), aliasImportChecker({ level: 'error' })],
-  // @source https://vitejs.dev/guide/build.html#library-mode
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
-      '@components': fileURLToPath(
-        new URL('./src/components', import.meta.url),
-      ),
-      '@hooks': fileURLToPath(new URL('./src/hooks', import.meta.url)),
-      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
-      '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
-      '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
-    },
-  },
+  plugins: [tsconfigPaths(), aliasImportChecker({ level: 'error' }), react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: ['src/__tests__/setup.ts'],
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
-      '@components': fileURLToPath(
-        new URL('./src/components', import.meta.url),
-      ),
-      '@hooks': fileURLToPath(new URL('./src/hooks', import.meta.url)),
-      '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
-      '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
-      '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
-    },
   },
 });
