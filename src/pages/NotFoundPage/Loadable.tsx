7f84d4bb0510e674d4dd4811f4479c842b2cfe73
Lyndon Baylin Jr. <lyndon@syntacticsinc.com>
2024-06-21 00:20:14 +0800

    chore(component): added `NotFoundPage` component

---
+++ src/pages/NotFoundPage/Loadable.tsx
@@ -0,0 +1,9 @@
+import { lazyLoad } from '@utils/loadable';
+
+export const NotFoundPage = lazyLoad(
+  () => import('./index'),
+  (module) => module.NotFoundPage,
+  {
+    fallback: <div>Loading...</div>,
+  },
+);
